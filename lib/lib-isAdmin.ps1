4f7c7d5c0148934509857042061decd735e90d64
Adam Clark <adamclerk@gmail.com>
2011-03-23 15:47:55 -0600

    lib-isAdmin.ps1, lib-menu.ps1

---
+++ lib/lib-isAdmin.ps1
@@ -0,0 +1,8 @@
+﻿#http://bradwilson.typepad.com/blog/powershell/
+function global:isAdmin()
+{
+	$windowsIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+	$windowsPrincipal = new-object 'System.Security.Principal.WindowsPrincipal' $windowsIdentity
+	 
+	return $windowsPrincipal.IsInRole("Administrators")
+}
---
+++ lib/lib-menu.ps1
@@ -0,0 +1,46 @@
+﻿#http://mspowershell.blogspot.com/2009/02/cli-menu-in-powershell.html
+function global:drawMenu {
+    ## supportfunction to the Menu function below
+    param ($menuItems, $menuPosition, $menuTitel)
+    $fcolor = $host.UI.RawUI.ForegroundColor
+    $bcolor = $host.UI.RawUI.BackgroundColor
+    $l = $menuItems.length + 1
+    cls
+    $menuwidth = $menuTitel.length + 4
+    Write-Host "`t" -NoNewLine
+    Write-Host ("*" * $menuwidth) -fore $fcolor -back $bcolor
+    Write-Host "`t" -NoNewLine
+    Write-Host "* $menuTitel *" -fore $fcolor -back $bcolor
+    Write-Host "`t" -NoNewLine
+    Write-Host ("*" * $menuwidth) -fore $fcolor -back $bcolor
+    Write-Host ""
+    Write-debug "L: $l MenuItems: $menuItems MenuPosition: $menuposition"
+    for ($i = 0; $i -le $l;$i++) {
+        Write-Host "`t" -NoNewLine
+        if ($i -eq $menuPosition) {
+            Write-Host "$($menuItems[$i])" -fore $bcolor -back $fcolor
+        } else {
+            Write-Host "$($menuItems[$i])" -fore $fcolor -back $bcolor
+        }
+    }
+}
+
+function global:menu {
+    ## Generate a small "DOS-like" menu.
+    ## Choose a menuitem using up and down arrows, select by pressing ENTER
+    param ([array]$menuItems, $menuTitel = "MENU")
+    $vkeycode = 0
+    $pos = 0
+    DrawMenu $menuItems $pos $menuTitel
+    While ($vkeycode -ne 13) {
+        $press = $host.ui.rawui.readkey("NoEcho,IncludeKeyDown")
+        $vkeycode = $press.virtualkeycode
+        Write-host "$($press.character)" -NoNewLine
+        If ($vkeycode -eq 38) {$pos--}
+        If ($vkeycode -eq 40) {$pos++}
+        if ($pos -lt 0) {$pos = 0}
+        if ($pos -ge $menuItems.length) {$pos = $menuItems.length -1}
+        drawMenu $menuItems $pos $menuTitel
+    }
+    Write-Output $($menuItems[$pos])
+}
